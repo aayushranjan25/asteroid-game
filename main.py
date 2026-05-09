f7ecfd414e69df5cf8b5b46121ff8ac458839607
aayushranjan25 <aayushranjan25@outlook.com>
2026-05-09 05:43:42 +0000

    added asteroids and shooting mechnanism

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from shot import Shot
 from player import Player
-from logger import log_state
+from logger import log_state, log_event
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
     print("Screen width:", SCREEN_WIDTH)
@@ -12,12 +16,17 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    shots = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (updatable, drawable, asteroids)
     Player.containers = (updatable, drawable)
+    Shot.containers = (updatable, drawable, shots)
+    AsteroidField.containers = (updatable,)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-
+    asteroid_field = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
@@ -26,6 +35,11 @@ def main():
             
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
